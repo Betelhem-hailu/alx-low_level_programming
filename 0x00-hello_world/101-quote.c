2a79b130f5735bdbd161088eb68ed2d1d504ccba
Betelhem-hailu <betelhemhailu811@gmail.com>
2022-03-10 14:27:05 -0800

    advanced

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,8 +6,8 @@
  */
 int main(void)
 {
-char ch[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	
-write(1, ch, sizeof(ch));
+
+write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59);
 return (1);
+
 }
