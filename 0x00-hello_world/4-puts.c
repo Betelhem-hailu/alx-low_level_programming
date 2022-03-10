811529bcc46a0d115f1ba14070980b0b00a370f7
Betelhem-hailu <betelhemhailu811@gmail.com>
2022-03-10 11:19:46 -0800

    hello,puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include<stdio.h>
 
+/**
+ * main block prints msg
+ * return 0(success)
+ */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzel");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
