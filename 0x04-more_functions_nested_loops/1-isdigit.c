cfc31308fd657b514826a35c70ac6653fa7a9e50
Betelhem-hailu <betelhemhailu811@gmail.com>
2022-03-17 11:02:05 +0300

    isdigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isupper - check the code
+ * @c : character is used
+ * Return: Always 0.
+ */
+ int _isigit(int c)
+ {
+  if (c >= '0' && c <= '9')
+  {
+      return (1);
+  }
+  else
+  {
+      return (0);
+  }
+ }
