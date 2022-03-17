371d646d98b7b265d70f7e890d12c68a20a30d4f
Betelhem-hailu <betelhemhailu811@gmail.com>
2022-03-17 11:05:36 +0300

    multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isupper - check the code
+ * @c : character is used
+ * Return: Always 0.
+ */
+ int mul(int a, int b)
+ {
+  int mul;
+
+  mul = a * b;
+  return (mul);
+ }
