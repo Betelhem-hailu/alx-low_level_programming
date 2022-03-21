d7f29059ad5b51c915519bbd4a635cc5df6f4e8d
Betelhem-hailu <betelhemhailu811@gmail.com>
2022-03-21 17:40:32 +0300

    stdout

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,1 +1,19 @@
 #include "main.h"
+/**
+ * _strlen - check the code
+ * @str :pointer
+ * Return: void.
+ */
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (*(s + i))
+    {
+        _putchar(*(s + i));
+        i++;
+    }
+		_putchar('\n');
+
+}
+
