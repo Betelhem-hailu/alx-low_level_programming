06a27b6d5636650f8fdd06f31ee61f133ec9e221
Betelhem-hailu <betelhemhailu811@gmail.com>
2022-03-28 14:37:43 +0300

    submit commit

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "ain.h"
 
 /**
  * *_memcpy - copies memory area
--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,4 +1,4 @@
-#include "ain.h"
+#include "main.h"
 
 /**
  * set_string - sets the value of a pointer to a char
